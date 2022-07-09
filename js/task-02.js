a447acba6876d525a67ea097c3e074f63021a086
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2022-07-09 14:28:45 +0300

    task1

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const createUl = document.createElement('ul')
+createUl.setAttribute('id', 'ingredients')
+
+const createLi = (count) => {
+  for (let i = 0; i < count; i += 1) {
+    const liEl = document.createElement('li')
+    liEl.classList = ('item') 
+    liEl.textContent = ingredients[i]
+    createUl.append(liEl)
+  }
+}
+createLi(6)
+
+console.log(createUl)
+
