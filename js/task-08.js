c9ac1642032fed1554b55a5f844ff8e479bb35c3
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2022-07-10 17:36:58 +0300

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
-
+const form = document.querySelector('.login-form')
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const [email, password] = event.target.elements
+    if(email.value.length === 0 || password.value.length === 0) {
+        alert('Всі поля повинні бути заповнені')
+    } else {
+        const fields = {email: email.value, password: password.value}
+        console.log(fields)
+        form.reset()
+    }
+})
